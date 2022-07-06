c9e7fe3c244ff727cfae9884e1a8cd9e0b73a49b
Kirill <kirill.novikov@gmial.com>
2022-07-07 00:22:19 +0300

    Example 005 Conditon If Else added

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter user name: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "masha") {
+    Console.WriteLine("Hurray, This is Masha!");    
+} else {
+    Console.Write("Hey, ");
+    Console.Write(userName);
+}
+
